f7968233375d0751f1d0b5147e777367905756c3
eyedeekay <hankhill19580@gmail.com>
2025-06-01 13:21:11 -0400

    start dg2 and dg3

---
+++ datagram2/doc.go
@@ -0,0 +1,8 @@
+package datagram2
+
+/*
+ * TODO: implement the Datagram2Session type for SAMv2 Datagram Sessions
+ * This package provides the implementation for datagram sessions
+ * using the SAMv2 protocol. It includes session management, datagram reading and writing,
+ * and integration with the SAMv2 protocol for secure communication.
+ */
---
+++ datagram3/doc.go
@@ -0,0 +1,8 @@
+package datagram3
+
+/*
+ * TODO: implement the Datagram2Session type for SAMv3 Authenticated Datagram Sessions
+ * This package provides the implementation for un-authenticated datagram sessions
+ * using the SAMv3 protocol. It includes session management, datagram reading and writing,
+ * and integration with the SAMv3 protocol for secure communication.
+ */
